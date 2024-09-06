bffd3d03f839595a9acf8fe81b5f7ec662bd6a3c
ANGELKUCANUL <169177450+ANGELKUCANUL@users.noreply.github.com>
2024-09-05 19:30:27 -0600

    Add files via upload

---
+++ aplicaciones web/home/Mangas/Mangas/Domain/Entities/Manga.cs
@@ -0,0 +1,10 @@
+namespace Mangas.Domain.Entities
+{
+    public class Manga
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+    }
+}
---
+++ aplicaciones web/home/Mangas/Mangas/Program.cs
@@ -0,0 +1,53 @@
+using Mangas.Services.Features.Mangas;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Agregar servicios al contenedor
+builder.Services.AddControllers(); // Agregar soporte para controladores
+builder.Services.AddSingleton<MangaService>(); // Registrar MangaService
+builder.Services.AddEndpointsApiExplorer(); // Para soportar Endpoints
+builder.Services.AddSwaggerGen(); // Para generar la documentación de Swagger
+
+var app = builder.Build();
+
+// Configurar la canalización de solicitudes HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+// Mapear controladores
+app.MapControllers();
+
+// Ejemplo de un endpoint simple
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+// Registro del WeatherForecast
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
